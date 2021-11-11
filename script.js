fc31c7c418a3f03e96c4b11518ed755c1d85b889
Csilla Pósa <posa.csilla@gmail.com>
2021-11-11 11:50:40 +0100

    Dark mode for nav and text box

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// Dark Mode Styles
+
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+}
 
 // Switch Theme Dynamically
 
@@ -11,8 +24,10 @@ function switchTheme(event) {
     if (event.target.checked) {
         //https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement
         document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
     }
 }
 
